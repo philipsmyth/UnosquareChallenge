19103d8a0f9d3a54961434db87401aab05178341
philipsmyth <130249497+philipsmyth@users.noreply.github.com>
2023-05-28 10:05:31 +0100

    Delete csharp directory

--- csharp/src/api/Models/Guess.cs
+++
@@ -1,6 +0,0 @@
-namespace api.Models;
-
-public class Guess
-{
-    public string Letter { get; set; }
-}
--- csharp/src/api/Models/ResponseError.cs
+++
@@ -1,6 +0,0 @@
-namespace api.Models;
-
-public class ResponseError
-{
-    public string Message { get; set; }
-}
