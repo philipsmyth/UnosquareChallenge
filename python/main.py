badbd00071fb4d53d5b5a7309c503af43969b4ed
philipsmyth <130249497+philipsmyth@users.noreply.github.com>
2023-05-28 10:05:55 +0100

    Delete python directory

--- python/main.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-from controllers.game import mod as game_module
-
-app = Flask(__name__)
-
-app.register_blueprint(game_module)
-
-if __name__ == '__main__':
-    app.run(debug=True, host="0.0.0.0", port=4567)
